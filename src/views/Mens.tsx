abfb7310c96829df235c76d0f39876ce99f38943
Bruno Bakarić <bak.13@hotmail.com>
2023-09-20 14:44:37 +0200

    Populate Mens component with products.

--- src/views/Mens.tsx
+++ src/views/Mens.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { useAppSelector, useAppDispatch } from '../hooks/hooks'
 import { fetchMensClothes } from '../store/features/menProducts/menProductsSlice'
+import AddToCartButton from '../components/AddToCartButton'
 
 const Mens = () => {
   const mensClothes = useAppSelector(state => state.mensClothes )
@@ -10,14 +12,28 @@ const Mens = () => {
   }, [dispatch])
 
   return (
-    <div>
-      <h2>List of Mens Clothes</h2>
-      
-        <ul>
-          {mensClothes.mensClothes.map(item => (
-            <li key={item.id}>{item.title}</li>
-          ))}
-        </ul>
+    <div className="products-wrapper">
+      <h1 className="products-wrapper__heading">
+        Men's Clothing
+      </h1>
+      <div className="products-wrapper__products-cards-wrapper">
+        {mensClothes.mensClothes.map(item => (
+          <div key={item.id} className="product-card">
+            <Link to={`/product/${item.id}`} className='products-wrapper__product-link'>
+              <div className="product-card__img-wrapper">
+                <img src={item.image} alt="product" className="product-card__product-img" />
+              </div>
+              <h3 className="product-card__product-name">
+                {item.title}
+              </h3>
+              <h4 className="product-card__product-price">
+                {item.price} €
+              </h4>
+            </Link>
+            <AddToCartButton />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
